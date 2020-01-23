54c42bc991c54705b00f891b6dd29389f9c404d8
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-01-23 17:21:03 +0100

    adding master_doc for RTD

--- docs/conf.py
+++ docs/conf.py
@@ -50,3 +50,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Added By PE -------------------------------------------------------------
+
+# Read The Doc uses a different version of sphinx by default, which has
+# a different default for master_doc
+master_doc = 'index'
