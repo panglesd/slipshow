cf069a3126ddcb59209bd507f3e0a56c4d5c690d
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-03-08 15:29:09 +0100

    re-adding example to dist

--- build/build.js
+++ build/build.js
@@ -5,4 +5,5 @@ shell.rm('-r', 'dist')
 // shell.exec('node-sass -r src/themes -o dist/themes')
 shell.mkdir("dist");
 shell.cp('-r', 'src/css', 'dist/css');
+shell.cp('-r', 'example', 'dist/example');
 shell.exec('rollup -c build/rollup.config.js');
