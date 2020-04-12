3052bb2bcd5164f23c26e9b2af3c6973389e7c09
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-04-12 17:57:03 +0200

    adding creation of tarball

--- build/build.js
+++ build/build.js
@@ -10,3 +10,5 @@ shell.cp('-r', 'src/css', 'dist/css');
 shell.cp('-r', 'src/scripts/generate.js', 'bin');
 shell.cp('-r', 'example', 'dist/example');
 shell.exec('rollup -c build/rollup.config.js');
+
+shell.exec('mkdir tmp; cd tmp; mkdir slipshow; cd slipshow; echo "{}" > package.json ; npm install slipshow; npm install mathjax@3; npx new-slipshow --mathjax > slipshow.html ; cd .. ; tar zcvf slipshow.tar.gz slipshow ; cd .. ; cp tmp/slipshow.tar.gz dist; rm -rf tmp ');
