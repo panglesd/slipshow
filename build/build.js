6295ccd89ca5305132c3f64b550f439ef7112a86
Paul-Elliot Anglès d'Auriac <peada@free.fr>
2020-04-12 16:02:39 +0200

    generation of template

--- build/build.js
+++ build/build.js
@@ -1,9 +1,12 @@
 var shell = require('shelljs')
 
 shell.rm('-r', 'dist')
+shell.rm('-r', 'bin')
 
 // shell.exec('node-sass -r src/themes -o dist/themes')
 shell.mkdir("dist");
+shell.mkdir("bin");
 shell.cp('-r', 'src/css', 'dist/css');
+shell.cp('-r', 'src/scripts/generate.js', 'bin');
 shell.cp('-r', 'example', 'dist/example');
 shell.exec('rollup -c build/rollup.config.js');
---
+++ src/scripts/generate.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+console.log("Hello World");
+console.log(process.argv)
+
+let template = '\n\
+<!DOCTYPE html>\n\
+<html lang="fr">\n\
+  <head>\n\
+    <meta charset="UTF-8">\n\
+\n\
+    <title>Slipshow</title>\n\
+\n';
+if(process.argv.includes("--cdn"))
+    template += '\
+'
+else
+    template += '\
+    <link rel="stylesheet" type="text/css" href="node_modules/slipshow/dist/css/slip.css">\n\
+    <link rel="stylesheet" type="text/css" href="node_modules/slipshow/dist/css/theorem.css">\n\
+'
+if(process.argv.includes("--mathjax-cdn"))
+    template += "<!-- TODO -->";
+if(process.argv.includes("--mathjax-local"))
+    template += '    <script src="node_modules/mathjax/es5/tex-chtml.js" id="MathJax-script" async></script>\n';
+
+template += '  <body>\n\
+\n\
+    <!-- This is the presentation -->\n\
+    <div class="root" id="rootSlip">\n\
+\n\
+      <!-- Add the slips here -->\n\
+\n\
+      <!-- For instance here is a slip -->\n\
+      <div class="slip" immediate-enter toc-title="Example of a slip">\n\
+	<div class="titre">The first slip of your presentation</div>\n\
+	<div class="slip-body-container">\n\
+          Example\n\
+	</div>\n\
+      </div>\n\
+      <!-- End of slip "Example of a slip" -->\n\
+\n\
+    </div>\n\
+\n\
+    <!-- Include the library -->\n\
+';
+if(process.argv.includes("--cdn"))
+    template += '\
+'
+else
+    template += '\
+    <script src="node_modules/slipshow/dist/slipshow.cdn.js"></script>\n\
+'
+template += '\
+    <!-- Start the presentation () -->\n\
+    <script>\n\
+      let engine = Slipshow.startSlipshow();\n\
+    </script>\n\
+  </body>\n\
+</html>\n\
+'
+;
+console.log(template)
+
