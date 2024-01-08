627cee8c8371bc101704372beb5425c4c83171be
Paul-Elliot <peada@free.fr>
2024-01-09 00:28:20 +0100

    Prepare for GH release with static binary
    Signed-off-by: Paul-Elliot <peada@free.fr>

---
+++ release/release.sh
@@ -0,0 +1,10 @@
+#/usr/bin/env bash
+set -xeuo pipefail
+
+archive_name=$OUTPUT/slipshow-$TARGETOS-$TARGETARCH.tar
+
+dune subst
+
+dune build -p slipshow
+# Executables are symlinks, follow with -h.
+tar hcf "$archive_name" -C _build/install/default bin/slipshow
