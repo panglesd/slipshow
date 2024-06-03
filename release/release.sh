32b86bb81ec574f493b0f3eabfdda786ba6c8955
Paul-Elliot <peada@free.fr>
2024-06-03 22:01:46 +0200

    Fix CI for Mac
    Signed-off-by: Paul-Elliot <peada@free.fr>

--- release/release.sh
+++ release/release.sh
@@ -5,7 +5,7 @@ archive_name=$OUTPUT/slipshow-$TARGETOS-$TARGETARCH.tar
 
 dune subst
 
-sed  -i 's/"()"/"(-cclib -static -cclib -no-pie)"/g' compiler/src/bin/static-linking-flags/static_linking_flags.ml
+sed  -i'' 's/"()"/"(-cclib -static -cclib -no-pie)"/g' compiler/src/bin/static-linking-flags/static_linking_flags.ml
 
 dune build -p slipshow
 # Executables are symlinks, follow with -h.
