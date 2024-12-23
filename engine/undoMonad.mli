9300811cf26ea8bf69930d8d8aea448ceed4f188
Paul-Elliot <peada@free.fr>
2024-12-23 11:14:06 +0100

    More UndoMonad simplifications

--- engine/undoMonad.mli
+++ engine/undoMonad.mli
@@ -2,7 +2,7 @@ type undo = unit -> unit Fut.t
 type 'a t = ('a * undo) Fut.t
 
 val bind : ('a -> 'b t) -> 'a t -> 'b t
-val return : 'a -> 'a t
+val return : ?undo:undo -> 'a -> 'a t
 val discard : 'a t -> 'a Fut.t
 
 module Syntax : sig
