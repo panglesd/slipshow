b8fb4e583c84d44578f20bc2a1f88016b2778f65
Paul-Elliot <peada@free.fr>
2025-01-21 14:14:01 +0100

    Previewing improvements

--- src/communication/communication.ml
+++ src/communication/communication.ml
@@ -3,5 +3,12 @@ open Sexplib.Std
 type payload = State of int | Ready [@@deriving sexp]
 type t = { id : string; payload : payload } [@@deriving sexp]
 
-let of_string s = s |> Sexplib.Sexp.of_string |> t_of_sexp
+let t_of_sexp_opt s =
+  try Some (t_of_sexp s) with Sexplib0.Sexp.Of_sexp_error _ -> None
+
+let of_string s =
+  match Sexplib.Sexp.of_string_conv s t_of_sexp_opt with
+  | `Result (Some _ as r) -> r
+  | _ -> None
+
 let to_string v = v |> sexp_of_t |> Sexplib.Sexp.to_string
