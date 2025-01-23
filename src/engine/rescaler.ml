66aa75ca83ee984798c3a0ba54c130d71f1b5ff8
Paul-Elliot <peada@free.fr>
2025-01-23 17:33:54 +0100

    Engine: Fix rescalers not listening to children resizes

--- src/engine/rescaler.ml
+++ src/engine/rescaler.ml
@@ -1,10 +1,17 @@
 open Brr
 
+(* We need to listen on resize for both slip-rescalers (the containers) as well as their only child. *)
 let setup_rescalers () =
   let slip_rescalers =
     El.fold_find_by_selector (fun x a -> x :: a) (Jstr.v ".slip-rescaler") []
   in
-  let rescale entry =
+  let slips =
+    List.filter_map
+      (fun e ->
+        match El.children ~only_els:true e with [ c ] -> Some c | _ -> None)
+      slip_rescalers
+  in
+  let rescaled_rescaler entry =
     match El.children ~only_els:true entry with
     | [ c ] ->
         let scale = El.inner_w entry /. El.inner_w c in
@@ -18,10 +25,20 @@ let setup_rescalers () =
             entry
     | [] | _ :: _ :: _ -> fun () -> Console.(log [ "problem!" ])
   in
+  let rescale entry =
+    if Brr.El.class' (Jstr.v "slip-rescaler") entry then rescaled_rescaler entry
+    else
+      match Brr.El.parent entry with
+      | None -> fun () -> ()
+      | Some parent -> rescaled_rescaler parent
+  in
   let callback entries _observer =
     entries
     |> List.map (fun entry -> rescale (ResizeObserver.Entry.target entry))
+    (* We need to do all the size computations at once, and then execute them,
+         otherwise they influence each others *)
     |> List.iter (fun f -> f ())
   in
   let observer = ResizeObserver.create callback in
-  List.iter (ResizeObserver.observe observer) slip_rescalers
+  List.iter (ResizeObserver.observe observer) slip_rescalers;
+  List.iter (ResizeObserver.observe observer) slips
