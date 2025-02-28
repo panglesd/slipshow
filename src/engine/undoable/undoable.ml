7c208ec580306a07dc1da83c061f92a2cdde00bf
Paul-Elliot <peada@free.fr>
2025-02-28 15:19:30 +0100

    Act on multiple elements

--- src/engine/undoable/undoable.ml
+++ src/engine/undoable/undoable.ml
@@ -19,3 +19,14 @@ module Syntax = struct
 end
 
 module Browser = Browser_
+
+module List = struct
+  open Syntax
+
+  let iter f l =
+    List.fold_left
+      (fun acc x ->
+        let> () = acc in
+        f x)
+      (return ()) l
+end
