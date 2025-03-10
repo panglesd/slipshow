825f3f8db55d9a1ecb8a1d227b5d421925190296
Paul-Elliot <peada@free.fr>
2025-03-10 21:57:14 +0100

    Build release assets in release mode

--- release/release.sh
+++ release/release.sh
@@ -5,6 +5,7 @@ archive_name=$OUTPUT/slipshow-$TARGETOS-$TARGETARCH.tar
 
 dune subst
 
-dune build -p slipshow
+dune build --profile release -p slipshow
+
 # Executables are symlinks, follow with -h.
 tar hcf "$archive_name" -C _build/install/default bin/slipshow
