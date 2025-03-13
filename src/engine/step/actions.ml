402b36de99fad9ed285ad233044e9351929eda25
Paul-Elliot <peada@free.fr>
2025-03-13 18:12:44 +0100

    Put Javascript API in its own module

--- src/engine/step/actions.ml
+++ src/engine/step/actions.ml
@@ -108,33 +108,8 @@ module AttributeActions = struct
       let arg =
         Jv.obj
           [|
-            ( "setStyle",
-              Jv.callback ~arity:3 @@ fun elem style value ->
-              let old_value =
-                let old_value = Brr.El.inline_style style elem in
-                if Jstr.equal old_value Jstr.empty then None else Some old_value
-              in
-              Brr.El.set_inline_style style value elem;
-              let undo _ =
-                Fut.return
-                @@
-                match old_value with
-                | None -> Brr.El.remove_inline_style style elem
-                | Some old_value -> Brr.El.set_inline_style style old_value elem
-              in
-              undos_ref := undo :: !undos_ref;
-              Jv.callback ~arity:1 undo );
-            ( "setClass",
-              Jv.callback ~arity:3 @@ fun elem class_ bool ->
-              let bool = Jv.to_bool bool in
-              Brr.Console.(log [ "set_class called with"; elem; class_; bool ]);
-              let old_value = Brr.El.class' class_ elem in
-              Brr.El.set_class class_ bool elem;
-              let undo _ =
-                Fut.return @@ Brr.El.set_class class_ old_value elem
-              in
-              undos_ref := undo :: !undos_ref;
-              Jv.callback ~arity:1 undo );
+            ("setStyle", Javascript_api.set_style undos_ref);
+            ("setClass", Javascript_api.set_class undos_ref);
           |]
       in
       let u = f arg in
