5da02f9bb8f33ad538fe512fc436c4b3cf3b857c
Paul-Elliot <peada@free.fr>
2025-06-15 21:20:00 +0200

    Add translate css property

--- src/engine/browser/css.mli
+++ src/engine/browser/css.mli
@@ -1,11 +1,13 @@
 type property =
   | Scale of float
   | Rotate of float
+  | Translate of { x : float; y : float }
   | Left of float
   | Right of float
   | Top of float
   | Bottom of float
   | TransitionDuration of float
+  | TransitionDelay of float
   | TransitionTiming of string
   | Width of float
   | Height of float
