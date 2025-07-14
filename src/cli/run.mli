7cd58939211142a42d5e5a69efa60afab2961a01
Paul-Elliot <peada@free.fr>
2025-07-14 18:27:45 +0200

    compatibility

--- src/cli/run.mli
+++ src/cli/run.mli
@@ -1,19 +1,19 @@
 val compile :
   input:[ `File of Fpath.t | `Stdin ] ->
   output:[ `File of Fpath.t | `Stdout ] ->
-  cli_frontmatter:Slipshow.Frontmatter.(unresolved t) ->
+  cli_frontmatter:Slipshow.Frontmatter.unresolved Slipshow.Frontmatter.t ->
   (Fpath.Set.t, [ `Msg of string ]) result
 
 val watch :
   input:Fpath.t ->
   output:Fpath.t ->
-  cli_frontmatter:Slipshow.Frontmatter.(unresolved t) ->
+  cli_frontmatter:Slipshow.Frontmatter.unresolved Slipshow.Frontmatter.t ->
   (unit, [ `Msg of string ]) result
 
 val serve :
   input:Fpath.t ->
   output:Fpath.t ->
-  cli_frontmatter:Slipshow.Frontmatter.(unresolved t) ->
+  cli_frontmatter:Slipshow.Frontmatter.unresolved Slipshow.Frontmatter.t ->
   (unit, [ `Msg of string ]) result
 
 val markdown_compile :
