f83e6ceefd7d2f68868642ecf8e7937df164df6f
Paul-Elliot <peada@free.fr>
2025-08-06 20:21:06 +0200

    Final order

--- src/engine/main.ml
+++ src/engine/main.ml
@@ -1,5 +1,12 @@
 let start ~width ~height ~id ~step =
   let open Fut.Syntax in
+  let* _ : (unit, _) result =
+    let window = Brr.G.window |> Brr.Window.to_jv in
+    let do_pdf = Jv.get window "slipshow__do_pdf" in
+    match Jv.defined do_pdf with
+    | true -> Jv.apply do_pdf [||] |> Fut.of_promise ~ok:(fun _ -> ())
+    | false -> Fut.return (Ok ())
+  in
   Constants.set_height height;
   Constants.set_width width;
   let el =
@@ -8,20 +15,10 @@ let start ~width ~height ~id ~step =
   let body = Brr.El.find_first_by_selector (Jstr.v "body") |> Option.get in
   let* () = Normalization.setup el in
   let* window = Universe.Window.setup el in
-  let* () =
-    Step.Action_scheduler.setup_pause_ancestors window () |> Undoable.discard
-  in
   (* TODO: move out of here (Later: Why?) *)
   let () = Rescale.setup_rescalers () in
   let () = Drawing.setup body in
   let () = Mouse_disappearing.setup () in
-  let* _ : (unit, _) result =
-    let window = Brr.G.window |> Brr.Window.to_jv in
-    let do_pdf = Jv.get window "slipshow__do_pdf" in
-    match Jv.defined do_pdf with
-    | true -> Jv.apply do_pdf [||] |> Fut.of_promise ~ok:(fun _ -> ())
-    | false -> Fut.return (Ok ())
-  in
   let initial_step =
     match step with
     | Some _ as step -> step
@@ -30,6 +27,9 @@ let start ~width ~height ~id ~step =
         |> Jstr.to_string |> int_of_string_opt
   in
   let _history = Browser.History.set_hash "" in
+  let* () =
+    Step.Action_scheduler.setup_pause_ancestors window () |> Undoable.discard
+  in
   (* We do one step first, without recording it/updating the hash, to enter in
      the first slip *)
   let* _ =
@@ -37,7 +37,11 @@ let start ~width ~height ~id ~step =
     |> Option.value ~default:(Undoable.return ())
     |> Undoable.discard
   in
-  let* () = Fut.tick ~ms:0 in
+  let* () =
+    (* For some reason, otherwise we things are not properly initialized during
+       the toc computation *)
+    Fut.tick ~ms:0
+  in
   let* () = Table_of_content.generate window el in
   let* () =
     match initial_step with
