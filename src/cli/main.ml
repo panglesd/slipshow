0b17421463f4f5e9663a91d1fa613e157db6936d
Paul-Elliot <peada@free.fr>
2025-08-07 13:59:17 +0200

    Update version title before release

--- src/cli/main.ml
+++ src/cli/main.ml
@@ -1,7 +1,7 @@
 open Cmdliner
 
 (* Update this on every release! *)
-let version_title = "The slides strike back"
+let version_title = "Plan 9 from External Files"
 
 let setup_log style_renderer level =
   Fmt_tty.setup_std_outputs ?style_renderer ();
