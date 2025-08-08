6229290f47b6fe2091d4ea2183598fab4b8f1c75
Paul-Elliot <peada@free.fr>
2025-08-08 10:20:46 +0200

    Trying a mac static link

--- src/cli/static-linking-flags/static_linking_flags.ml
+++ src/cli/static-linking-flags/static_linking_flags.ml
@@ -2,5 +2,6 @@ let () =
   print_endline
   @@
   match Sys.argv.(1) with
-  | "aarch64-unknown-linux-musl" | "x86_64-pc-linux-musl" -> "()"
-  | _ -> "()"
+  | "aarch64-unknown-linux-musl" | "x86_64-pc-linux-musl" ->
+      "(-cclib -static -cclib -no-pie)"
+  | _ -> "(-cclib -static -cclib -no-pie)"
