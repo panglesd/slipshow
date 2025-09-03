9dd4c0773ac8884c978fbfbba181200afd3b7d64
Paul-Elliot <peada@free.fr>
2025-09-03 09:22:35 +0200

    Squashed 'vendor/github.com/panglesd/perfect-freehand-ocaml/' changes from 37630f9..f65e0c8
    f65e0c8 Add options getters (and reformat)
    099b846 WIP
    
    git-subtree-dir: vendor/github.com/panglesd/perfect-freehand-ocaml
    git-subtree-split: f65e0c8ccbee92a46b9b6d012c757bab6d0eea20

--- lib/perfect_freehand.ml
+++ lib/perfect_freehand.ml
@@ -1,12 +1,13 @@
 let get_stroke = Jv.get Jv.global "getStroke"
 let get_svg_path_from_stroke = Jv.get Jv.global "getSvgPathFromStroke"
+
 module Point = struct
   type t = Jv.Jarray.t
-  let v x y = Jv.of_list Jv.of_float [ x ; y]
 
-            let get_x v = Jv.Jarray.get v 0 |> Jv.to_float
-            let get_y v = Jv.Jarray.get v 1
- |> Jv.to_float
+  let v x y = Jv.of_list Jv.of_float [ x; y ]
+  let get_x v = Jv.Jarray.get v 0 |> Jv.to_float
+  let get_y v = Jv.Jarray.get v 1 |> Jv.to_float
+
   include (Jv.Id : Jv.CONV with type t := t)
 end
 
@@ -14,21 +15,34 @@ module Options = struct
   type t = Jv.t
 
   let v ?size ?thinning ?smoothing ?streamline ?last () =
-    Jv.obj [|
+    Jv.obj
+      [|
         ("size", Jv.of_option ~none:Jv.undefined Jv.of_float size);
-        ("thinning", Jv.of_option ~none:Jv.undefined  Jv.of_float thinning);
-        ("smoothing", Jv.of_option ~none:Jv.undefined  Jv.of_float smoothing);
-        ("streamline", Jv.of_option  ~none:Jv.undefined   Jv.of_float streamline);
-        ("last", Jv.of_option  ~none:Jv.undefined   Jv.of_bool last);
+        ("thinning", Jv.of_option ~none:Jv.undefined Jv.of_float thinning);
+        ("smoothing", Jv.of_option ~none:Jv.undefined Jv.of_float smoothing);
+        ("streamline", Jv.of_option ~none:Jv.undefined Jv.of_float streamline);
+        ("last", Jv.of_option ~none:Jv.undefined Jv.of_bool last);
       |]
+
+  let size t = Jv.get t "size" |> Jv.to_option Jv.to_float
+  let thinning t = Jv.get t "thinning" |> Jv.to_option Jv.to_float
+  let smoothing t = Jv.get t "smoothing" |> Jv.to_option Jv.to_float
+  let streamline t = Jv.get t "streamline" |> Jv.to_option Jv.to_float
+  let last t = Jv.get t "last" |> Jv.to_option Jv.to_bool
+
   include (Jv.Id : Jv.CONV with type t := t)
 end
+
 let get_stroke ?options x =
   let x = Jv.of_list Point.to_jv x in
-  let res =  match options with None ->  Jv.apply get_stroke [|x|] | Some options ->  Jv.apply get_stroke [|x; options|] in
+  let res =
+    match options with
+    | None -> Jv.apply get_stroke [| x |]
+    | Some options -> Jv.apply get_stroke [| x; options |]
+  in
   Jv.to_list Point.of_jv res
 
 let get_svg_path_from_stroke x =
   let x = Jv.of_list Point.to_jv x in
-  let res =   Jv.apply get_svg_path_from_stroke [|x|] in
+  let res = Jv.apply get_svg_path_from_stroke [| x |] in
   Jv.to_jstr res
--- lib/perfect_freehand.mli
+++ lib/perfect_freehand.mli
@@ -16,21 +16,32 @@ module Point : sig
   (**/**)
 end
 
-
-module Options :sig
+module Options : sig
   type t
 
-  val v : ?size:float -> ?thinning:float -> ?smoothing:float -> ?streamline:float -> ?last:bool -> unit -> t
-   
+  val v :
+    ?size:float ->
+    ?thinning:float ->
+    ?smoothing:float ->
+    ?streamline:float ->
+    ?last:bool ->
+    unit ->
+    t
+
+  val size : t -> float option
+  val thinning : t -> float option
+  val smoothing : t -> float option
+  val streamline : t -> float option
+  val last : t -> bool option
+
   (**/**)
 
   include Jv.CONV with type t := t
 
   (**/**)
-  end
+end
 
 val get_stroke : ?options:Options.t -> Point.t list -> Point.t list
 (* TODO: options *)
 
 val get_svg_path_from_stroke : Point.t list -> Jstr.t
-  
