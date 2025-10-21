95920ba16a577d5fb6ffb18a4039ee072787c4e1
Paul-Elliot <peada@free.fr>
2025-10-21 10:04:46 +0200

    Document previous commit

--- src/engine/runtime/universe/state.ml
+++ src/engine/runtime/universe/state.ml
@@ -3,6 +3,11 @@ open Constants
 let coordinates = ref None
 let set_coord v = coordinates := Some v
 
+(* We do this trick instead of starting with coordinates being directly the
+   "starting" value since [width] and [height] may not have been set correctly
+   at the time the ref is initialized.
+
+   Imperative programming is confusing! *)
 let get_coord () =
   match !coordinates with
   | None -> { Coordinates.x = width () /. 2.; y = height () /. 2.; scale = 1. }
