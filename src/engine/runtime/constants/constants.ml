361bf8e464e3cff6cd34a23d2209adb99f298a5c
Paul-Elliot <peada@free.fr>
2025-10-21 10:04:46 +0200

    Document previous commit

--- src/engine/runtime/constants/constants.ml
+++ src/engine/runtime/constants/constants.ml
@@ -1,3 +1,12 @@
+(* Be very careful when using this that the width and height need to have been
+   initialized. Otherwise, they have their default value, which is likely wrong.
+
+   If you use them in a function, that's probably right: setting them is one of
+   the first thing slipshow does. What can go wrong is if we access them as a
+   toplevel. See [universe/state.ml] for an example.
+
+   Imperative programming is confusing!
+*)
 let width = ref 1440.
 let height = ref 1080.
 let set_width = ( := ) width
--- src/engine/runtime/constants/constants.mli
+++ src/engine/runtime/constants/constants.mli
@@ -1,3 +1,13 @@
+(** Be very careful when using this that the width and height need to have been
+    initialized. Otherwise, they have their default value, which is likely
+    wrong.
+
+    If you use them in a function, that's probably right: setting them is one of
+    the first thing slipshow does. What can go wrong is if we access them as a
+    toplevel. See [universe/state.ml] for an example.
+
+    Imperative programming is confusing! *)
+
 val set_width : float -> unit
 val set_height : float -> unit
 val width : unit -> float
