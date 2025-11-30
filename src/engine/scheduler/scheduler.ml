6c7d914bbcab8fd9c58be6842c5c62fe874473e8
Paul-Elliot <peada@free.fr>
2025-11-30 12:08:31 -0500

    Refactor and stop clone view when mirroring

--- src/engine/scheduler/scheduler.ml
+++ src/engine/scheduler/scheduler.ml
@@ -88,11 +88,12 @@ let html =
       display: flex;
     }
     #speaker-view {
-      width:60%;
+      width:100%;
     }
     #speaker-notes {
-      width:40%;
+      width:35%;
       overflow: scroll;
+      flex-shrink:0;
     }
     </style>
   </body>
@@ -179,54 +180,9 @@ let open_window handle_msg =
               (Jstr.v "#slipshow__mirror-view video")
             |> Option.get |> Brr_io.Media.El.of_el
           in
-          let _unlisten =
-            Brr.Ev.listen Brr.Ev.click
-              (fun _ ->
-                let open Fut.Syntax in
-                let _ : unit Fut.t =
-                  let ( !! ) = Jstr.v in
-                  Brr.El.set_class !!"clone-mode" false
-                    (Brr.Document.body (Brr.Window.document child));
-                  Brr.El.set_class !!"mirror-mode" true
-                    (Brr.Document.body (Brr.Window.document child));
-                  let child_navigator =
-                    Jv.get (Brr.Window.to_jv child) "navigator"
-                    |> Brr.Navigator.of_jv
-                  in
-                  let devices =
-                    Brr_io.Media.Devices.of_navigator child_navigator
-                  in
-                  let constraints = Brr_io.Media.Stream.Constraints.av () in
-                  let+ media =
-                    Brr_io.Media.Devices.get_display_media devices constraints
-                  in
-                  match media with
-                  | Ok stream ->
-                      let provider =
-                        Brr_io.Media.El.Provider.of_media_stream stream
-                      in
-                      Brr_io.Media.El.set_src_object mirror_video
-                        (Some provider)
-                  | Error e -> Brr.Console.(error [ e ])
-                in
-                ())
-              (Brr.El.as_target mirror_button)
-          in
-          let _unlisten =
-            Brr.Ev.listen Brr.Ev.click
-              (fun _ ->
-                let ( !! ) = Jstr.v in
-                Brr.El.set_class !!"clone-mode" true
-                  (Brr.Document.body (Brr.Window.document child));
-                Brr.El.set_class !!"mirror-mode" false
-                  (Brr.Document.body (Brr.Window.document child));
-                let tracks =
-                  let stream = Brr_io.Media.El.capture_stream mirror_video in
-                  Brr_io.Media.Stream.get_tracks stream
-                in
-                List.iter (fun t -> Brr_io.Media.Track.stop t) tracks;
-                Brr_io.Media.El.set_src_object mirror_video None)
-              (Brr.El.as_target clone_button)
+          let () =
+            View_mode.setup ~speaker_view:child ~video_el:mirror_video
+              ~mirror_button ~clone_button ~child_iframe ~src
           in
           let timer =
             Brr.El.find_first_by_selector ~root:el (Jstr.v "#timer")
---
+++ src/engine/scheduler/view_mode.mli
@@ -0,0 +1,8 @@
+val setup :
+  speaker_view:Brr.El.window ->
+  video_el:Brr_io.Media.El.t ->
+  mirror_button:Brr.El.t ->
+  clone_button:Brr.El.t ->
+  child_iframe:Brr.El.t ->
+  src:Jstr.t ->
+  unit
