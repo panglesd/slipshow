decff374057720fcab501a4006d9a947eeb00e12
Paul-Elliot <peada@free.fr>
2026-02-24 00:38:22 +0100

    Fix "Wrong type" error

--- src/diagnosis/diagnosis.mli
+++ src/diagnosis/diagnosis.mli
@@ -5,7 +5,7 @@ val loc_of_ploc : loc -> Actions_arguments.Parse.loc -> loc
 type t =
   | DuplicateID of { id : string; occurrences : loc list }
   | MissingFile of { file : string; error_msg : string; locs : loc list }
-  | WrongType of { loc_reason : loc; loc_block : loc }
+  | WrongType of { loc_reason : loc; loc_block : loc; expected_type : string }
   | ParsingError of { action : string; msg : string; loc : loc }
   | ParsingWarnor of { warnor : Actions_arguments.Parse.warnor; loc : loc }
   | MissingID of { id : string; loc : loc }
