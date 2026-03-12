b83a6b4cac6c5518cd33174b21f65f4649e0fc67
Paul-Elliot <peada@free.fr>
2026-03-12 15:49:26 +0100

    Set new version name

--- src/cli/main.ml
+++ src/cli/main.ml
@@ -1,7 +1,7 @@
 open Cmdliner
 
 (* Update this on every release! *)
-let version_title = "Manim Black"
+let version_title = "Don't look {up}"
 let slipshow_version = "%%VERSION%%: " ^ version_title
 
 let setup_log style_renderer level =
