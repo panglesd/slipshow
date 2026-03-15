fbedd3e06ba3f4645efa8cc5b1b3915d50725887
Paul-Elliot <peada@free.fr>
2026-03-15 19:17:04 +0100

    Server: refactor
    Merge `compile` and `k` which had no reason to be separated, I think.
    Split in modules.

--- src/server/slipshow_server.mli
+++ src/server/slipshow_server.mli
@@ -4,8 +4,6 @@ val do_serve :
   ( (Slipshow.delayed * string (* warnings as string *)) * Fpath.Set.t,
     [ `Msg of string ] )
   result) ->
-  (unit, [ `Msg of string ]) result
+  unit
 
-val do_watch :
-  (unit -> (Fpath.Set.t, [ `Msg of string ]) result) ->
-  (unit, [ `Msg of string ]) result
+val do_watch : (unit -> (Fpath.Set.t, [ `Msg of string ]) result) -> unit
