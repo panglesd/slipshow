3d9084ea9ee023e1d1cf60459f105b8e55c2bc17
Paul-Elliot <peada@free.fr>
2026-03-16 19:11:46 +0100

    Add a visual indication if the preview is connected to the server

--- src/server/client/client.ml
+++ src/server/client/client.ml
@@ -18,6 +18,9 @@ let warnings =
 let warnings_show =
   El.find_first_by_selector (Jstr.v "#warnings-slipshow-show") |> Option.get
 
+let connection_show =
+  El.find_first_by_selector (Jstr.v "#connection-slipshow") |> Option.get
+
 let _unlistener =
   Ev.listen Ev.click
     (fun _ ->
@@ -45,17 +48,27 @@ let previewer =
   Previewer.create_previewer ?initial_stage ~callback ~include_speaker_view:true
     ~errors_el:warnings ~steal_focus:true elem
 
+let ( !! ) = Jstr.v
+
+let set_connected () =
+  El.set_class !!"connected" true connection_show;
+  El.set_class !!"disconnected" false connection_show
+
+let set_disconnected () =
+  El.set_class !!"connected" false connection_show;
+  El.set_class !!"disconnected" true connection_show
+
 let rec do_and_retry f arg =
   let open Fut.Syntax in
   let* x = f arg in
   match x with
   | Ok x -> Fut.return x
   | Error e ->
+      set_disconnected ();
       Console.error [ e ];
-      let* () = Fut.tick ~ms:5000 in
+      let* () = Fut.tick ~ms:3000 in
       do_and_retry f arg
 
-let ( !! ) = Jstr.v
 let version = ref ""
 
 let recv () =
@@ -65,7 +78,11 @@ let recv () =
     let+ raw_data =
       let open Brr_io.Fetch in
       let abort = Abort.controller () in
-      let timeout = G.set_timeout ~ms:10000 @@ fun () -> Abort.abort abort in
+      let timeout =
+        G.set_timeout ~ms:10000 @@ fun () ->
+        set_disconnected ();
+        Abort.abort abort
+      in
       let signal = Abort.signal abort in
       let body = Body.of_jstr !!(!version) in
       let init = Request.init ~method':!!"post" ~signal ~body () in
@@ -77,11 +94,12 @@ let recv () =
     in
     let data = Proto.of_string (Jstr.to_string raw_data) in
     match data with
-    | None -> () (* TODO *)
+    | None -> set_disconnected ()
     | Some Pong ->
         Console.log [ "pong" ];
-        () (* TODO *)
+        set_connected ()
     | Some (Update data) -> (
+        set_connected ();
         version := data.version;
         let data = Slipshow.string_to_delayed data.content in
         match data with
