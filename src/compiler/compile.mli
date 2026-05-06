49ec91375eeb7834dbfe30a1ad949729a892b23d
Paul-Elliot <peada@free.fr>
2026-05-06 22:30:45 +0200

    Unexpose `Compile.of_cmarkit`
    It is an internal function

--- src/compiler/compile.mli
+++ src/compiler/compile.mli
@@ -7,16 +7,7 @@ type t = {
   action_plan : Action_plan.t;
 }
 
-val of_cmarkit :
-  read_file:file_reader ->
-  fm:Frontmatter.t ->
-  Cmarkit.Doc.t ->
-  t
-
 val to_cmarkit : Ast.t -> Cmarkit.Doc.t
 
 val compile :
-  ?file:string ->
-  ?read_file:file_reader ->
-  string ->
-  t * Diagnosis.t list
+  ?file:string -> ?read_file:file_reader -> string -> t * Diagnosis.t list
