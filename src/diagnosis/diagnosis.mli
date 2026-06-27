d111b8823e9128e722f837f9351db9a24fb455ab
Paul-Elliot <peada@free.fr>
2026-06-27 16:05:26 +0200

    Diagnosis: check if we are speaking about a draw file later

--- src/diagnosis/diagnosis.mli
+++ src/diagnosis/diagnosis.mli
@@ -5,7 +5,6 @@ val loc_of_ploc : loc -> Actions_arguments.W.loc -> loc
 type t =
   | DuplicateID of { id : string; occurrences : loc list }
   | MissingFile of { file : string; error_msg : string; locs : loc list }
-  | MissingDrawFile of { file : string; error_msg : string; locs : loc list }
   | WrongType of { loc_reason : loc; loc_block : loc; expected_type : string }
   | ParsingError of { action : string; msg : string; loc : loc }
   | ParsingWarnor of { warnor : Actions_arguments.W.warnor; loc : loc }
