47e223a6a430fe56acb3431b58c741c4004474a0
Paul-Elliot <peada@free.fr>
2026-08-04 16:03:07 +0200

    Add mli to current_ast

---
+++ src/lspishow/current_ast.mli
@@ -0,0 +1,25 @@
+type key_trace =
+  | Key of string Cmarkit.node * Cmarkit.Attributes.value Cmarkit.node option
+  | Value of string Cmarkit.node * Cmarkit.Attributes.value Cmarkit.node
+  | Class of string Cmarkit.node
+  | Id of string Cmarkit.node
+
+
+type attribute_trace = Cmarkit.Attributes.t * key_trace option
+type inline_trace = Cmarkit.Inline.t list
+type block_trace = Cmarkit.Block.t list
+
+type trace = {
+  attribute : attribute_trace option;
+  inline : inline_trace;
+  block : block_trace;
+}
+
+val get_leave : path:Fpath.t -> Linol_lwt.Position.t -> Cmarkit.Doc.t -> trace
+
+val get_target :
+  path:Fpath.t ->
+  Linol_lwt.Position.t ->
+  Slipshow.Ast.Action_plan.t ->
+  string option
+(** Finds if we are in an action's argument that would correspond to a target *)
