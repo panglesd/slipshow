f27f7441fab816b406f5848510d7259b9f9020d5
Paul-Elliot <peada@free.fr>
2026-08-06 15:40:13 +0200

    WIP

--- src/engine/runtime/drawing/drawing_state/drawing_state.ml
+++ src/engine/runtime/drawing/drawing_state/drawing_state.ml
@@ -3,21 +3,11 @@ let now () = Brr.Performance.now_ms Brr.G.performance
 include Types
 module Json = Json
 module Path_editing = Path_editing
+module Messages = Messages
 
 let can_save = Lwd.var false
 
-let workspaces : workspaces =
-  { recordings = Lwd_table.make (); live_drawing = Lwd_table.make () }
-
-let editing_tool = Lwd.var Select
-let current_replaying_state : replaying_state option Lwd.var = Lwd.var None
-
-let live_drawing_state =
-  {
-    tool = Lwd.var Pointer;
-    color = Lwd.var "blue";
-    width = Lwd.var Width.medium;
-  }
+include State
 
 module Status : sig
   val set : status -> unit
