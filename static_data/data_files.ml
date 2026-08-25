f9cb15f6ca3c41e59cca3bb475abeba2bfc73439
Paul-Elliot <peada@free.fr>
2026-08-25 23:08:22 +0200

    Remove last use of ocaml-crunch in data_files

--- static_data/data_files.ml
+++ static_data/data_files.ml
@@ -7,19 +7,11 @@ type file =
   | Favicon
   | Mathjax_js
 
-let string_of_file = function
-  | Mathjax_js -> "tex-svg-full.js.crunch"
-  | _ -> assert false
-
 let read f = match f with
   | Slipshow_js -> [%blob "../src/engine/runtime/slipshow.js"]
   | Scheduler_js -> [%blob "../src/engine/scheduler/scheduler.bc.js"]
   | Slip_internal_css -> [%blob "../src/engine/runtime/slipshow-internal.css"]
   | Slip_system_css -> [%blob "../src/engine/runtime/slipshow-system.css"]
   | Pdf_support -> [%blob "../src/engine/pdf-support/pdf_support.bc.js"]
+  | Mathjax_js -> [%blob "tex-svg-full.js"]
   | Favicon -> [%blob "../logo/favicon.ico"]
-  | _ ->
-     Data_contents.read (string_of_file f)
-     |> function
-       | Some c -> c
-       | None -> assert false
