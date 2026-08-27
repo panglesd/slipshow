panglesd/slipshow

commit 326f4798b711fda4821e8d094772e014f813094c

Prevent double gui_id attributes

src/compiler/compile.ml
@@ -268,70 +268,72 @@ module Stage1 = struct
     in
     match res with [] -> None | res -> Some (Block.Blocks (res, meta))
 
-  let map_attrs ~get_gui_id ~embed_loc = function
-    | `Kv (("up", m), v) -> [ `Kv (("up-at-unpause", m), v) ]
-    | `Kv (("center", m), v) -> [ `Kv (("center-at-unpause", m), v) ]
-    | `Kv (("down", m), v) -> [ `Kv (("down-at-unpause", m), v) ]
-    | `Kv (("exec", m), v) -> [ `Kv (("exec-at-unpause", m), v) ]
-    | `Kv (("scroll", m), v) -> [ `Kv (("scroll-at-unpause", m), v) ]
-    | `Kv (("enter", m), v) -> [ `Kv (("enter-at-unpause", m), v) ]
-    | `Kv (("emph", m), v) -> [ `Kv (("emph-at-unpause", m), v) ]
-    | `Kv (("focus", m), v) -> [ `Kv (("focus-at-unpause", m), v) ]
-    | `Kv (("reveal", m), v) -> [ `Kv (("reveal-at-unpause", m), v) ]
-    | `Kv (("static", m), v) -> [ `Kv (("static-at-unpause", m), v) ]
-    | `Kv (("unemph", m), v) -> [ `Kv (("unemph-at-unpause", m), v) ]
-    | `Kv (("unfocus", m), v) -> [ `Kv (("unfocus-at-unpause", m), v) ]
-    | `Kv (("unreveal", m), v) -> [ `Kv (("unreveal-at-unpause", m), v) ]
-    | `Kv (("unstatic", m), v) -> [ `Kv (("unstatic-at-unpause", m), v) ]
-    (* TODO: Improve this (eg by moving it to another phase) *)
-    | `Kv (("children:up", m), v) -> [ `Kv (("children:up-at-unpause", m), v) ]
-    | `Kv (("children:center", m), v) ->
-        [ `Kv (("children:center-at-unpause", m), v) ]
-    | `Kv (("children:down", m), v) ->
-        [ `Kv (("children:down-at-unpause", m), v) ]
-    | `Kv (("children:exec", m), v) ->
-        [ `Kv (("children:exec-at-unpause", m), v) ]
-    | `Kv (("children:scroll", m), v) ->
-        [ `Kv (("children:scroll-at-unpause", m), v) ]
-    | `Kv (("children:enter", m), v) ->
-        [ `Kv (("children:enter-at-unpause", m), v) ]
-    | `Kv (("children:emph", m), v) ->
-        [ `Kv (("children:emph-at-unpause", m), v) ]
-    | `Kv (("children:focus", m), v) ->
-        [ `Kv (("children:focus-at-unpause", m), v) ]
-    | `Kv (("children:reveal", m), v) ->
-        [ `Kv (("children:reveal-at-unpause", m), v) ]
-    | `Kv (("children:static", m), v) ->
-        [ `Kv (("children:static-at-unpause", m), v) ]
-    | `Kv (("children:unemph", m), v) ->
-        [ `Kv (("children:unemph-at-unpause", m), v) ]
-    | `Kv (("children:unfocus", m), v) ->
-        [ `Kv (("children:unfocus-at-unpause", m), v) ]
-    | `Kv (("children:unreveal", m), v) ->
-        [ `Kv (("children:unreveal-at-unpause", m), v) ]
-    | `Kv (("children:unstatic", m), v) ->
-        [ `Kv (("children:unstatic-at-unpause", m), v) ]
-    | `Kv ((gui_s, meta), _) as gui when String.equal Special_attrs.gui gui_s ->
-        let id_attr =
-          let v = get_gui_id () in
-          { Cmarkit.Attributes.v; delimiter = Some '"' }
-        in
-        let file_attr =
-          let v = meta |> Cmarkit.Meta.textloc |> Cmarkit.Textloc.file in
-          { Cmarkit.Attributes.v; delimiter = Some '"' }
-        in
-        let original_loc =
-          if embed_loc then
-            [
-              `Kv ((Special_attrs.gui_id, Meta.none), Some (id_attr, Meta.none));
-              `Kv
-                ( (Special_attrs.gui_file, Meta.none),
-                  Some (file_attr, Meta.none) );
-            ]
-          else []
-        in
-        gui :: original_loc
-    | x -> [ x ]
+  let map_attrs ~get_gui_id ~embed_loc attrs =
+    let map_attrs attrs = function
+      | `Kv (("up", m), v) -> [ `Kv (("up-at-unpause", m), v) ]
+      | `Kv (("center", m), v) -> [ `Kv (("center-at-unpause", m), v) ]
+      | `Kv (("down", m), v) -> [ `Kv (("down-at-unpause", m), v) ]
+      | `Kv (("exec", m), v) -> [ `Kv (("exec-at-unpause", m), v) ]
+      | `Kv (("scroll", m), v) -> [ `Kv (("scroll-at-unpause", m), v) ]
+      | `Kv (("enter", m), v) -> [ `Kv (("enter-at-unpause", m), v) ]
+      | `Kv (("emph", m), v) -> [ `Kv (("emph-at-unpause", m), v) ]
+      | `Kv (("focus", m), v) -> [ `Kv (("focus-at-unpause", m), v) ]
+      | `Kv (("reveal", m), v) -> [ `Kv (("reveal-at-unpause", m), v) ]
+      | `Kv (("static", m), v) -> [ `Kv (("static-at-unpause", m), v) ]
+      | `Kv (("unemph", m), v) -> [ `Kv (("unemph-at-unpause", m), v) ]
+      | `Kv (("unfocus", m), v) -> [ `Kv (("unfocus-at-unpause", m), v) ]
+      | `Kv (("unreveal", m), v) -> [ `Kv (("unreveal-at-unpause", m), v) ]
+      | `Kv (("unstatic", m), v) -> [ `Kv (("unstatic-at-unpause", m), v) ]
+      (* TODO: Improve this (eg by moving it to another phase) *)
+      | `Kv (("children:up", m), v) ->
+          [ `Kv (("children:up-at-unpause", m), v) ]
+      | `Kv (("children:center", m), v) ->
+          [ `Kv (("children:center-at-unpause", m), v) ]
+      | `Kv (("children:down", m), v) ->
+          [ `Kv (("children:down-at-unpause", m), v) ]
+      | `Kv (("children:exec", m), v) ->
+          [ `Kv (("children:exec-at-unpause", m), v) ]
+      | `Kv (("children:scroll", m), v) ->
+          [ `Kv (("children:scroll-at-unpause", m), v) ]
+      | `Kv (("children:enter", m), v) ->
+          [ `Kv (("children:enter-at-unpause", m), v) ]
+      | `Kv (("children:emph", m), v) ->
+          [ `Kv (("children:emph-at-unpause", m), v) ]
+      | `Kv (("children:focus", m), v) ->
+          [ `Kv (("children:focus-at-unpause", m), v) ]
+      | `Kv (("children:reveal", m), v) ->
+          [ `Kv (("children:reveal-at-unpause", m), v) ]
+      | `Kv (("children:static", m), v) ->
+          [ `Kv (("children:static-at-unpause", m), v) ]
+      | `Kv (("children:unemph", m), v) ->
+          [ `Kv (("children:unemph-at-unpause", m), v) ]
+      | `Kv (("children:unfocus", m), v) ->
+          [ `Kv (("children:unfocus-at-unpause", m), v) ]
+      | `Kv (("children:unreveal", m), v) ->
+          [ `Kv (("children:unreveal-at-unpause", m), v) ]
+      | `Kv (("children:unstatic", m), v) ->
+          [ `Kv (("children:unstatic-at-unpause", m), v) ]
+      | `Kv ((gui_s, meta), _) as gui when String.equal Special_attrs.gui gui_s
+        ->
+          let if_not_already_defined key value =
+            match Cmarkit.Attributes.find key attrs with
+            | None when embed_loc ->
+                let v = value () in
+                let value = { Cmarkit.Attributes.v; delimiter = Some '"' } in
+                [ `Kv ((key, Meta.none), Some (value, Meta.none)) ]
+            | _ -> []
+          in
+          let id_attr =
+            if_not_already_defined Special_attrs.gui_id @@ get_gui_id
+          in
+          let file_attr =
+            if_not_already_defined Special_attrs.gui_file @@ fun () ->
+            meta |> Cmarkit.Meta.textloc |> Cmarkit.Textloc.file
+          in
+          gui :: (id_attr @ file_attr)
+      | x -> [ x ]
+    in
+    Cmarkit.Attributes.map (map_attrs attrs) attrs
 
   let execute ~get_gui_id ~htbl_include ~embed_loc current_path defs =
     let ret x = `Map x in
@@ -402,9 +404,7 @@ module Stage1 = struct
         match fm.Frontmatter.global.toplevel_attributes with
         | None -> None
         | Some (attrs, meta) ->
-            Some
-              ( Cmarkit.Attributes.map (map_attrs ~get_gui_id ~embed_loc) attrs,
-                meta )
+            Some ((map_attrs ~get_gui_id ~embed_loc) attrs, meta)
       in
       {
         fm with
@@ -880,14 +880,15 @@ let to_cmarkit units =
     | Ast.S_inline i -> inline m i
     | _ -> Mapper.default
   in
-  let attrs = function
-    | `Kv (("up-at-unpause", m), v) -> [ `Kv (("up", m), v) ]
-    | `Kv (("center-at-unpause", m), v) -> [ `Kv (("center", m), v) ]
-    | `Kv (("enter-at-unpause", m), v) -> [ `Kv (("enter", m), v) ]
-    | `Kv (("down-at-unpause", m), v) -> [ `Kv (("down", m), v) ]
-    | `Kv (("exec-at-unpause", m), v) -> [ `Kv (("exec", m), v) ]
-    | `Kv (("scroll-at-unpause", m), v) -> [ `Kv (("scroll", m), v) ]
-    | x -> [ x ]
+  let attrs =
+    Cmarkit.Attributes.map (function
+      | `Kv (("up-at-unpause", m), v) -> [ `Kv (("up", m), v) ]
+      | `Kv (("center-at-unpause", m), v) -> [ `Kv (("center", m), v) ]
+      | `Kv (("enter-at-unpause", m), v) -> [ `Kv (("enter", m), v) ]
+      | `Kv (("down-at-unpause", m), v) -> [ `Kv (("down", m), v) ]
+      | `Kv (("exec-at-unpause", m), v) -> [ `Kv (("exec", m), v) ]
+      | `Kv (("scroll-at-unpause", m), v) -> [ `Kv (("scroll", m), v) ]
+      | x -> [ x ])
   in
   Ast.Mapper.make ~block ~inline ~attrs ()
 

vendor/github.com/panglesd/cmarkit/src/cmarkit.ml
@@ -3512,14 +3512,7 @@ module Mapper = struct
     { inline_ext_default : Inline.t map;
       block_ext_default : Block.t map;
       inline : Inline.t mapper;
-      attrs :
-        ([ `Class of string node
-         | `Id of string node
-         | `Kv of Attributes.key node * Attributes.value node option ] ->
-         [ `Class of string node
-         | `Id of string node
-         | `Kv of Attributes.key node * Attributes.value node option ]
-           list);
+      attrs : Attributes.t -> Attributes.t;
       block : Block.t mapper }
   and 'a map = t -> 'a -> 'a filter_map
   and 'a mapper = t -> 'a -> 'a result
@@ -3527,7 +3520,7 @@ module Mapper = struct
   let none _ _ = `Default
   let ext_inline_none _ _ = invalid_arg Inline.err_unknown
   let ext_block_none _ _ = invalid_arg Block.err_unknown
-  let attrs x = [x]
+  let attrs x = x
   let make
       ?(inline_ext_default = ext_inline_none)
       ?(block_ext_default = ext_block_none)
@@ -3542,13 +3535,13 @@ module Mapper = struct
 
   let ( let* ) = Option.bind
 
-  let map_attrs m attrs = Attributes.map m.attrs attrs
+  let map_attrs m attrs = m.attrs attrs
 
   let rec map_inline m i = match m.inline m i with
   | `Map i -> i
   | `Default ->
       let open Inline in
-     let map_attrs (attrs, meta) = Attributes.map m.attrs attrs, meta in
+     let map_attrs (attrs, meta) = m.attrs attrs, meta in
       match i with
       | Break _ | Raw_html _ as i -> Some i
       | Autolink ((al, attrs), meta) ->
@@ -3589,7 +3582,7 @@ module Mapper = struct
       | Ext_attrs ({ content; attrs }, meta) ->
          let attrs =
            let attrs, meta = attrs in
-           Attributes.map m.attrs attrs, meta
+           m.attrs attrs, meta
          in
          let content = map_inline m content in
          let content = Option.value ~default:(Inline.Inlines ([], Meta.none)) content in
@@ -3599,7 +3592,7 @@ module Mapper = struct
   let rec map_block m b = match m.block m b with
   | `Map b -> b
   | `Default ->
-     let map_attrs (attrs, meta) = Attributes.map m.attrs attrs, meta in
+     let map_attrs (attrs, meta) = m.attrs attrs, meta in
       let open Block in
       match b with
       | Blank_line _ as b -> Some b
@@ -3681,7 +3674,7 @@ module Mapper = struct
         let block = map_block m (Block.Footnote.block fn) in
         Block.Footnote.Def (({ fn with block }, attrs), meta)
     | Block.Attribute_definition.Def ((label, attrs), meta) ->
-        let attrs = Attributes.map m.attrs attrs in
+        let attrs = m.attrs attrs in
         Block.Attribute_definition.Def ((label, attrs), meta)
     | def -> def
     in

vendor/github.com/panglesd/cmarkit/src/cmarkit.mli
@@ -1579,13 +1579,8 @@ module Mapper : sig
 
   val make :
     ?inline_ext_default:Inline.t map -> ?block_ext_default:Block.t map ->
-    ?inline:Inline.t mapper -> ?block:Block.t mapper -> ?attrs:([ `Class of string node
-        | `Id of string node
-        | `Kv of Attributes.key node * Attributes.value node option ] ->
-        [ `Class of string node
-        | `Id of string node
-        | `Kv of Attributes.key node * Attributes.value node option ] list) ->
- unit -> t
+    ?inline:Inline.t mapper -> ?block:Block.t mapper ->
+    ?attrs:(Attributes.t -> Attributes.t) -> unit -> t
   (** [make ?inline ?block ()] is a mapper using [inline] and [block]
       to map the abstract syntax tree. Both default to [fun _ _ -> `Default].
 

commit 92dc260086fa9acad5b9b3e14021b58e7f6fa43b

Fix missing attribute
This adds also a small difference in behavior in terms of reported errors: a
duplicated attribute will be ignored and so not be reported if
non-standard. This is fine as anyway the attribute eraser will be reported.

The regression was introduced by 326f4798, and before it was working but mostly
by chance.

src/compiler/ast.ml
@@ -191,10 +191,9 @@ module Utils = struct
 
     (** Get the attributes of a cmarkit node, returns them and the element
         stripped of its attributes *)
-    let merge_attribute new_attrs b =
+    let merge_attribute ~keep_base new_attrs b =
       let merge (base, meta) =
-        (Attributes.merge ~base ~new_attrs, meta)
-        (* Old attributes take precendence over "new" one *)
+        (Attributes.merge ~keep_base ~base ~new_attrs, meta)
       in
       match update_attribute merge b with None -> b | Some (b, _) -> b
 
@@ -299,10 +298,9 @@ module Utils = struct
 
     (** Get the attributes of a cmarkit node, returns them and the element
         stripped of its attributes *)
-    let merge_attribute new_attrs b =
+    let merge_attribute ~keep_base new_attrs b =
       let merge (base, meta) =
-        (Attributes.merge ~base ~new_attrs, meta)
-        (* Old attributes take precendence over "new" one *)
+        (Attributes.merge ~keep_base ~base ~new_attrs, meta)
       in
       match update_attribute merge b with None -> b | Some (b, _) -> b
 

src/compiler/compile.ml
@@ -177,7 +177,8 @@ module Stage1 = struct
       | None -> None
       | Some ((ld, (attrs_ld, meta2)), meta) ->
           let attrs =
-            Cmarkit.Attributes.merge ~base:attrs ~new_attrs:attrs_ld
+            Cmarkit.Attributes.merge ~keep_base:true ~base:attrs
+              ~new_attrs:attrs_ld
           in
           let kind = classify_link_definition ld attrs in
           let attrs_ld = Mapper.map_attrs m attrs_ld in
@@ -353,10 +354,10 @@ module Stage1 = struct
                   { Cmarkit.Attributes.v; delimiter = Some '"' }
                 in
                 ( attrs
-                  |> Cmarkit.Attributes.add
+                  |> Cmarkit.Attributes.add ~keep_base:true
                        (Special_attrs.gui_file, Meta.none)
                        (Some (file_attr, Meta.none))
-                  |> Cmarkit.Attributes.add
+                  |> Cmarkit.Attributes.add ~keep_base:true
                        (Special_attrs.gui_id, Meta.none)
                        (Some (id_attr, Meta.none)),
                   meta ))
@@ -441,14 +442,20 @@ module Stage2 = struct
                 | Some key -> (
                     match (categorize key, value) with
                     | `Class c, None -> Attributes.add_class acc (c, meta)
-                    | `Kv c, _ -> Attributes.add (c, meta) value acc
+                    | `Kv c, _ ->
+                        Attributes.add ~keep_base:false (c, meta) value acc
                     | `Class c, Some (_, v_meta) ->
                         Diagnosis.add
                           (ChildrenClassWithValue { loc = Meta.textloc v_meta });
                         Attributes.add_class acc (c, meta)))
               Attributes.empty kvs
           in
-          let bs = List.map (Ast.Utils.Block.merge_attribute new_attrs) bs in
+          let bs =
+            (* keep_base true to avoid erasing more specific attributes *)
+            List.map
+              (Ast.Utils.Block.merge_attribute ~keep_base:true new_attrs)
+              bs
+          in
           let bs =
             match Mapper.map_block m (Block.Blocks (bs, m_bs)) with
             | None -> Block.Blocks ([], m_bs)
@@ -497,7 +504,10 @@ module Stage3 = struct
             || Attributes.mem Actions_arguments.Enter.on attrs)
             || not may_enter
           then attrs
-          else Attributes.add (Actions_arguments.Enter.on, Meta.none) None attrs
+          else
+            Attributes.add ~keep_base:true
+              (Actions_arguments.Enter.on, Meta.none)
+              None attrs
         in
         let attrs = Mapper.map_attrs m attrs in
         (b, (attrs, meta2))
@@ -880,16 +890,16 @@ let to_cmarkit units =
     | Ast.S_inline i -> inline m i
     | _ -> Mapper.default
   in
-  let attrs =
-    Cmarkit.Attributes.map (function
-      | `Kv (("up-at-unpause", m), v) -> [ `Kv (("up", m), v) ]
-      | `Kv (("center-at-unpause", m), v) -> [ `Kv (("center", m), v) ]
-      | `Kv (("enter-at-unpause", m), v) -> [ `Kv (("enter", m), v) ]
-      | `Kv (("down-at-unpause", m), v) -> [ `Kv (("down", m), v) ]
-      | `Kv (("exec-at-unpause", m), v) -> [ `Kv (("exec", m), v) ]
-      | `Kv (("scroll-at-unpause", m), v) -> [ `Kv (("scroll", m), v) ]
-      | x -> [ x ])
+  let attrs = function
+    | `Kv (("up-at-unpause", m), v) -> [ `Kv (("up", m), v) ]
+    | `Kv (("center-at-unpause", m), v) -> [ `Kv (("center", m), v) ]
+    | `Kv (("enter-at-unpause", m), v) -> [ `Kv (("enter", m), v) ]
+    | `Kv (("down-at-unpause", m), v) -> [ `Kv (("down", m), v) ]
+    | `Kv (("exec-at-unpause", m), v) -> [ `Kv (("exec", m), v) ]
+    | `Kv (("scroll-at-unpause", m), v) -> [ `Kv (("scroll", m), v) ]
+    | x -> [ x ]
   in
+  let attrs = Attributes.map attrs in
   Ast.Mapper.make ~block ~inline ~attrs ()
 
 let to_cmarkit

src/compiler/frontmatter.ml
@@ -73,7 +73,9 @@ module Global = struct
         (match (x.toplevel_attributes, y.toplevel_attributes) with
         | Some (a1, meta1), Some (a2, _meta2) ->
             (* Hopefully not merging the locations is fine *)
-            Some (Cmarkit.Attributes.merge ~base:a1 ~new_attrs:a2, meta1)
+            Some
+              ( Cmarkit.Attributes.merge ~keep_base:false ~base:a1 ~new_attrs:a2,
+                meta1 )
         | (Some _ as a), _ | _, (Some _ as a) -> a
         | None, None -> None);
     }
@@ -132,7 +134,7 @@ module Attributes = struct
       | None -> v
       | Some (a, _meta2) ->
           (* Hopefully not merging the locations is fine *)
-          Cmarkit.Attributes.merge ~base:a ~new_attrs:v
+          Cmarkit.Attributes.merge ~keep_base:false ~base:a ~new_attrs:v
     in
     { fm with local = { attributes = Some (v, meta1) } }
 end
@@ -162,7 +164,7 @@ module Toplevel_attributes = struct
       | None -> v
       | Some (a, _meta2) ->
           (* Hopefully not merging the locations is fine *)
-          Cmarkit.Attributes.merge ~base:a ~new_attrs:v
+          Cmarkit.Attributes.merge ~keep_base:false ~base:a ~new_attrs:v
     in
     {
       fm with

src/compiler/renderers.ml
@@ -212,8 +212,9 @@ let svg c ~uri ~files i attrs =
   match src with
   | `Link l ->
       let attrs =
-        attrs |> add_escaped_attrs "data" l
-        |> add_escaped_attrs "type" "image/svg+xml"
+        attrs
+        |> add_escaped_attrs ~keep_base:false "data" l
+        |> add_escaped_attrs ~keep_base:false "type" "image/svg+xml"
       in
       media ~self_closing:false ~media_name:"object" c ~uri ~files i attrs
   | `Source (content, _mime_type) ->
@@ -241,9 +242,10 @@ let pure_embed ~root c ~name uri files attrs =
           in
           let attrs =
             attrs
-            |> add_escaped_attrs "x-path" (Fpath.to_string p)
-            |> add_escaped_attrs "x-data" (Option.value ~default:"" content)
-            |> add_escaped_attrs "x-name" name
+            |> add_escaped_attrs ~keep_base:false "x-path" (Fpath.to_string p)
+            |> add_escaped_attrs ~keep_base:false "x-data"
+                 (Option.value ~default:"" content)
+            |> add_escaped_attrs ~keep_base:false "x-name" name
           in
           Context.string c "<span";
           RenderAttrs.add_attrs c attrs;
@@ -404,7 +406,7 @@ let custom_html_renderer (units : Ast.units)
           true
       | Ast.SlipScript ((cb, (attrs, _)), _) ->
           let attrs =
-            Attributes.add ("type", Meta.none)
+            Attributes.add ~keep_base:false ("type", Meta.none)
               (Some ({ v = "slip-script"; delimiter = None }, Meta.none))
               attrs
           in

vendor/github.com/panglesd/cmarkit/src/cmarkit.ml
@@ -148,11 +148,15 @@ module Attributes = struct
 
   let class' t = t.class'
 
-  let add_class t class' = { t with class' = class' :: t.class'}
+  let add_class t class' =
+    if
+      List.exists (fun (cl, _meta) -> String.equal cl (fst class')) t.class'
+    then t
+    else { t with class' = class' :: t.class'}
 
   let remove_class t class' =
     { t with
-      class' = List.filter (fun (c, _) -> compare class' c <> 0) t.class'
+      class' = List.filter (fun (c, _) -> not (String.equal class' c)) t.class'
     }
 
   (** {1 Id}) *)
@@ -170,15 +174,27 @@ module Attributes = struct
   let mem key t =
     List.exists (function ((k, _), _) -> String.equal k key) t.kv_attributes
 
-  let add (key, meta) value t =
+  let add ~keep_base (key, meta) value t =
     match key, value with
     | "id", Some ({v = value; _}, meta) ->
-       set_id t (value, meta)
+        (match id t with
+        | Some _ when keep_base -> t
+        | _ -> set_id t (value, meta)
+        )
     | "class", Some ({v = value; _}, meta) ->
        let values = String.split_on_char ' ' value in
        List.fold_left (fun t value -> add_class t (value, meta)) t values
     | _ ->
-       let kv_attributes = ((key, meta), value) :: t.kv_attributes in
+       let mem_key = mem key t in
+       if keep_base && mem_key then t else
+       let kv_attributes =
+         if mem_key then
+           List.map
+             (function | ((k, _), _) when String.equal k key -> ((key, meta), value)
+                       | x -> x)
+           t.kv_attributes
+         else ((key, meta), value) :: t.kv_attributes
+       in
        { t with kv_attributes }
 
   let remove key t =
@@ -190,10 +206,17 @@ module Attributes = struct
   let find key t =
     List.find_opt (function ((k, _), _) -> String.equal k key) t.kv_attributes
 
-  let merge ~base ~new_attrs =
-    let base = match id new_attrs with None -> base | Some id -> set_id base id in
+  let merge ~keep_base ~base ~new_attrs =
+    let base =
+      match id base, id new_attrs with
+      | Some _, _ when keep_base -> base
+      | _, None -> base
+      | _, Some id -> set_id base id
+    in
     let base = List.fold_left add_class base (class' new_attrs) in
-    List.fold_left (fun base (k, v) -> add k v base) base (kv_attributes new_attrs)
+    List.fold_left
+      (fun base (k, v) -> add ~keep_base k v base)
+      base (kv_attributes new_attrs)
 
   (** Merge *)
 
@@ -205,7 +228,7 @@ module Attributes = struct
       (fun acc -> function
         | `Class x -> add_class acc x
         | `Id id -> set_id acc id
-        | `Kv (x,y) -> add x y acc)
+        | `Kv (x,y) -> add ~keep_base:false x y acc)
       empty (class' @ id @ kv_attrs)
 end
 
@@ -1779,7 +1802,7 @@ module Inline_struct = struct
               let v, meta = attr_of_rev_spans p value in
               Some ({Attributes.v ; delimiter}, meta)
          in
-         Attributes.add key value attrs
+         Attributes.add ~keep_base:false key value attrs
     in
     let attrs = List.fold_right add_attribute new_attrs attrs in
     let first = p.current_char and last = p.current_line_last_char in
@@ -2852,7 +2875,7 @@ module Block_struct = struct
               let (v, meta) = Inline_struct.attr_of_rev_spans p value in
               Some ({Attributes.v ; delimiter}, meta)
          in
-         Attributes.add key value attrs
+         Attributes.add ~keep_base:false key value attrs
     in
     let new_attrs =
       List.fold_right add_attribute new_attrs Attributes.empty, Meta.none
@@ -2883,7 +2906,7 @@ module Block_struct = struct
               let (v, meta) =  (Inline_struct.attr_of_rev_spans p value) in
               Some ({Attributes.v ; delimiter}, meta)
          in
-         Attributes.add key value attrs
+         Attributes.add ~keep_base:false key value attrs
     in
     let attrs = List.fold_right add_attribute new_attrs attrs in
     let first = p.current_char and last = p.current_line_last_char in

vendor/github.com/panglesd/cmarkit/src/cmarkit.mli
@@ -313,7 +313,7 @@ module Attributes : sig
   val mem : key -> t -> bool
   (** [mem k m] is [true] iff [k] is bound in [m]. *)
 
-  val add : key node -> value node option -> t -> t
+  val add : keep_base:bool -> key node -> value node option -> t -> t
   (** [add k v m] is [m] with key [k] bound to [v]. *)
 
   val remove : key -> t -> t
@@ -335,7 +335,7 @@ module Attributes : sig
        list) ->
     t -> t
 
-  val merge : base:t -> new_attrs:t -> t
+  val merge : keep_base:bool -> base:t -> new_attrs:t -> t
 end
 
 type 'a attributed = 'a * Attributes.t node

vendor/github.com/panglesd/cmarkit/src/cmarkit_html.ml
@@ -264,7 +264,9 @@ let link_dest_and_title c ld =
 let image ?(close = " >") c i attrs =
   match Inline.Link.reference_definition (C.get_defs c) i with
   | Some (Link_definition.Def ((ld, (attributes, _)), _)) ->
-     let attributes = Attributes.merge ~base:attributes ~new_attrs:attrs in
+      let attributes =
+        Attributes.merge ~keep_base:false ~base:attributes ~new_attrs:attrs
+      in
       let plain_text c i =
         let lines = Inline.to_plain_text ~break_on_soft:false i in
         String.concat "\n" (List.map (String.concat "") lines)
@@ -303,7 +305,9 @@ let link_footnote c l fn =
 
 let link c l attrs = match Inline.Link.reference_definition (C.get_defs c) l with
 | Some (Link_definition.Def ((ld, (attributes, _)), _)) ->
-    let attributes = Attributes.merge ~base:attributes ~new_attrs:attrs in
+    let attributes =
+      Attributes.merge ~keep_base:false ~base:attributes ~new_attrs:attrs
+    in
     let link, title = link_dest_and_title c ld in
     C.string c "<a href=\""; pct_encoded_string c link;
     C.string c "\"";

test/compiler/dash-separator/children-slides.t/run.t
@@ -20,11 +20,6 @@
    24 │  {children:key=value #attributes}
       │   ^^^^^^^^^^^^ 
   
-  warning: Non standard attribute: 'key'
-      ┌─ slides.md:24:2
-   24 │  {children:key=value #attributes}
-      │   ^^^^^^^^^^^^ 
-  
 
   $ show_source slides.html | htmlq -p "#slides"
   
@@ -85,7 +80,7 @@
           This is a second slide</span>
       </p>
     </div>
-    <div k="v" key="value">
+    <div k="v" key="other value">
       <p>
         <span>
           Hello!</span>