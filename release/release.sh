16c584a13e3f4f5fbf1de40cb0b0484bc6d5d71d
Paul-Elliot <peada@free.fr>
2026-09-05 00:27:48 +0200

    Fix version subst in CI

--- release/release.sh
+++ release/release.sh
@@ -5,6 +5,14 @@ archive_name=$OUTPUT/slipshow-$TARGETOS-$TARGETARCH.tar
 
 dune subst
 
+# I've had sufficiently many issues with version to check. Hardcoding the file
+# name is not satisfactory though.
+if grep -q '%%VERSION%%' src/version/slipshow_version.ml; then
+  echo "Error: the version watermarks were not substituted." >&2
+  echo "(\`dune subst\` needs the git repository and its tags.)" >&2
+  exit 1
+fi
+
 dune build --profile release -p slipshow
 
 mkdir -p $OUTPUT
