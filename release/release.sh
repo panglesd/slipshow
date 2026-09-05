5878a6622f05cf347903e78ab9153f250ca81b79
Paul-Elliot <peada@free.fr>
2026-09-05 10:03:22 +0200

    Good check on version
    The %%VERSION%% of release.sh was substituted...

--- release/release.sh
+++ release/release.sh
@@ -7,16 +7,17 @@ dune subst
 
 dune build --profile release -p slipshow
 
-echo $(./_build/install/default/bin/slipshow --version)
+version=$(./_build/install/default/bin/slipshow --version)
+echo "$version"
 
-# I've had sufficiently many issues with version to check. Hardcoding the file
-# name is not satisfactory though.
-if grep -q '%%VERSION%%' src/version/slipshow_version.ml; then
-  echo "Error: the version watermarks were not substituted." >&2
-  echo "(\`dune subst\` needs the git repository and its tags.)" >&2
-  exit 1
-fi
-
+# I've had sufficiently many issues with version to check.
+case "$version" in
+  *%%*)
+    echo "Error: the version watermarks were not substituted: $version" >&2
+    echo "(\`dune subst\` needs the git repository and its tags.)" >&2
+    exit 1
+    ;;
+esac
 
 mkdir -p $OUTPUT
 
